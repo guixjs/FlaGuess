94c6ec67ea2c7a0a64c711a18e1559410bd17098
Guilherme Felix <f3lixguilherme@gmail.com>
2025-01-30 12:03:12 -0300

    correção no JavaScript

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
+let dicas = 1;
+const btnDica = document.querySelector(".btnDica")
 
-async function obterLista(){
+
+const form = document.querySelector(".form")
+
+form.addEventListener("submit",(evento)=>{
+    evento.preventDefault()
+})
+
+async function sortearPais(){
     const response = await fetch(`https://restcountries.com/v3.1/all`)
     const listaPaises = await response.json()
-    return listaPaises
-}
+    const indexSorteado = Math.floor(Math.random() * listaPaises.length);
+    const pais = listaPaises[indexSorteado];
 
-async function sortearPais() {
-    const listaPaises = await obterLista();
-    const indexSorteado = Math.floor(Math.random()*listaPaises.length);
-    return listaPaises[indexSorteado];
-}
-async function mostrarPais(){
-    const pais = await sortearPais()
+    console.log(pais)
     const infoPais = {
         bandeira: pais.flags.png,
         area: pais.area,
@@ -21,18 +24,27 @@ async function mostrarPais(){
         continente: pais.continents[0],
         capital: pais.capital[0]
     }
-    mostrarDicas(infoPais)
+    return infoPais
 }
 
-async function mostrarDicas(infoPais) {
+async function mostrarDicas() {
+    const infoPais = await sortearPais()
+    
     document.getElementById("area").textContent = `Área (em Km): ${infoPais.area}`
     document.getElementById("populacao").textContent = `Total de habitantes: ${infoPais.populacao}`
-    document.querySelector("img").src = infoPais.bandeira
+
+
     document.getElementById("moeda").textContent = `Símbolo da moeda local: ${infoPais.moeda}`
     document.getElementById("idioma").textContent = `Idioma(s): ${infoPais.idioma}`
+
+
     document.getElementById("continente").textContent = `Continente: ${infoPais.continente}`
     document.getElementById("capital").textContent = `Capital: ${infoPais.capital}`
+
+
+    document.querySelector("img").src = infoPais.bandeira
+  
     
 }
 
-mostrarPais()
+mostrarDicas()
