e2cbb1384308f4666721764e87a19117a262d75f
Guilherme Felix <f3lixguilherme@gmail.com>
2025-02-02 19:09:06 -0300

    corrigindo funções no Js, para que o fetch só seja feito uma vez, além de iniciar a interatividade com o usuário

--- script.js
+++ script.js
@@ -1,28 +1,56 @@
-function iconBloquearBandeira(texto){
-    const btnTeste = document.getElementById("iconFlag")
-    btnTeste.innerText = texto
-    // progress_activity - QUANDO ESTIVER CARREGANDO
-    //lock - quando estiver bloqueado
+// ELEMENTOS DO HTML
+const btnDica = document.querySelector(".btnDica")
+const form = document.querySelector(".form")
+const input = document.getElementById("input")
+const btnMostrarResposta = document.querySelector(".btnRes")
+
+function desistir(){
+    input.value = paisDaVez.nome
 }
 
-let dicas = 1;
-const btnDica = document.querySelector(".btnDica")
-
-
-const form = document.querySelector(".form")
-
+//impede a página de recarregar e chama a verificarResposta
 form.addEventListener("submit",(evento)=>{
     evento.preventDefault()
+    console.log(input.value)
+    nome = paisDaVez.nome.toLowerCase()
+    if(input.value == paisDaVez.nome){
+        document.querySelector("h2").textContent = "ACERTOU"
+        mostrarDicas()
+    }else{
+        document.querySelector("h2").textContent = "ERROU"
+    }
+    
+    input.value = " "
 })
 
-async function sortearPais(){
-    const response = await fetch(`https://restcountries.com/v3.1/all`)
-    const listaPaises = await response.json()
-    const indexSorteado = Math.floor(Math.random() * listaPaises.length);
-    const pais = listaPaises[indexSorteado];
+// VARIAVEIS
+let listaPaises = []
+let paisDaVez = null
+let indexRepetidos = []
 
-    console.log(pais)
-    const infoPais = {
+
+async function carregarPaises() {
+    const response = await fetch(`https://restcountries.com/v3.1/all`)
+    listaPaises = await response.json()
+    return listaPaises
+}
+
+async function sortearPais(){
+    if(listaPaises.length == 0){
+        listaPaises = await carregarPaises()
+    }
+    const indexSorteado = Math.floor(Math.random() * listaPaises.length);
+
+    indexRepetidos.forEach(repetidos => {
+        if(indexSorteado == repetidos){
+            indexSorteado = Math.floor(Math.random() * listaPaises.length);
+        }
+    });
+    const pais = listaPaises[indexSorteado];
+    indexRepetidos.push(indexSorteado)
+    
+    paisSorteado = {
+        nome: Object.values(pais.translations.por)[1],
         bandeira: pais.flags.png,
         area: pais.area,
         populacao: pais.population,
@@ -31,13 +59,17 @@ async function sortearPais(){
         continente: pais.continents[0],
         capital: pais.capital[0]
     }
-    return infoPais
+    // verificarResposta()
+    paisDaVez = paisSorteado
+    return paisSorteado
 }
+
 
 async function mostrarDicas() {
     const infoPais = await sortearPais()
     
-    document.getElementById("area").textContent = `Área (em Km): ${infoPais.area}`
+    
+    document.getElementById("area").textContent = `Área (em Km²): ${infoPais.area}`
     document.getElementById("populacao").textContent = `Total de habitantes: ${infoPais.populacao}`
 
 
@@ -45,13 +77,24 @@ async function mostrarDicas() {
     document.getElementById("idioma").textContent = `Idioma(s): ${infoPais.idioma}`
 
 
-    document.getElementById("continente").textContent = `Continente: ${infoPais.continente}`
     document.getElementById("capital").textContent = `Capital: ${infoPais.capital}`
+    document.getElementById("continente").textContent = `Continente: ${infoPais.continente}`
 
-
-    document.querySelector("img").src = infoPais.bandeira
-  iconBloquearBandeira(" ")
     
+    const flag = document.querySelector("img");
+
+    flag.onload = function() {
+        document.getElementById("iconFlag").style.display = "none";
+    };
+    
+    flag.src = infoPais.bandeira;
 }
 
-mostrarDicas()
+
+
+async function teste() {
+    await mostrarDicas()
+    await console.log(paisDaVez)
+}
+teste()
+
